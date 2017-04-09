af8315fe01a1455449461c66d4d6dee0eb7e9a45
damnboy19 <ceze02@student.coppin.edu>
2017-04-09 11:29:23 -0400

    Add files via upload

---
+++ multiplying.js
@@ -0,0 +1,4 @@
+//creating the variable names
+var add = 8, sub = 9, tot = add * sub, word = "the total if multiplied is ";
+//get element to print
+document.getElementById("content").textContent = word + tot;
